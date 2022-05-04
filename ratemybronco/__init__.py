bb5aae2b5eaa1a64c3cf06e9e590ffdf6c11942d
Sara Nersisian <saranersesian@gmail.com>
2022-05-03 19:08:32 -0700

    Add About us page

--- ratemybronco/__init__.py
+++ ratemybronco/__init__.py
@@ -16,9 +16,10 @@ import os
 app = Flask(__name__)
 
 
-## MySQL connector and set up
+# MySQL connector and set up
 # ratemybroncoDB = mysql.connector.connect(option_files="ratemybronco/config.cfg", option_groups="database")
-ratemybroncoDB = mysql.connector.connect(user='admin', password='ratemybronco', host='ratemybronco.cckwxul93z9y.us-west-1.rds.amazonaws.com', port=3306, database='ratemybronco')
+ratemybroncoDB = mysql.connector.connect(user='admin', password='ratemybronco',
+                                         host='ratemybronco.cckwxul93z9y.us-west-1.rds.amazonaws.com', port=3306, database='ratemybronco')
 mycursor = ratemybroncoDB.cursor()
 
 
@@ -31,58 +32,62 @@ def landing():
 # Search page routing
 @app.route("/search", methods=["GET", "POST"])
 def search():
-  cards = {}
-  query = request.args.get("query") # get attribute query, must be named "query"
-  instructor = False
+    cards = {}
+    # get attribute query, must be named "query"
+    query = request.args.get("query")
+    instructor = False
 
-  if query:
-    # if it has numbers it is a class otherwise prof's name
-    instructor = not any(i.isdigit() for i in query)
-    instructorid = []
+    if query:
+        # if it has numbers it is a class otherwise prof's name
+        instructor = not any(i.isdigit() for i in query)
+        instructorid = []
 
-    print("searching for instructors") if instructor else print("searching for classes")
-    if instructor:
-      parsed_query = query.split() # assuming user inputs first and last name correctly
-      fname = parsed_query[0]
-      lname = parsed_query[1]
+        print("searching for instructors") if instructor else print(
+            "searching for classes")
+        if instructor:
+            parsed_query = query.split()  # assuming user inputs first and last name correctly
+            fname = parsed_query[0]
+            lname = parsed_query[1]
 
-      # returns id of the instructor to look up the courses
-      # assumes there is only one prof with this name
-      mycursor.callproc("searchInstructor", args=(fname, lname))
-      for result in mycursor.stored_results():
-        res = result.fetchone()
-        instructorid = res[0]
+            # returns id of the instructor to look up the courses
+            # assumes there is only one prof with this name
+            mycursor.callproc("searchInstructor", args=(fname, lname))
+            for result in mycursor.stored_results():
+                res = result.fetchone()
+                instructorid = res[0]
 
-      # get courses only requires instructor id.
-      mycursor.callproc("getCourses", args=(instructorid,))
-      for result in mycursor.stored_results():
-        for res in result:
-          # name every card with the course ID
-          cards[str(res[0])] = {'ProfessorName': query, 'CourseName': res[1], 'CourseDesc': res[2]}
+            # get courses only requires instructor id.
+            mycursor.callproc("getCourses", args=(instructorid,))
+            for result in mycursor.stored_results():
+                for res in result:
+                    # name every card with the course ID
+                    cards[str(res[0])] = {
+                        'ProfessorName': query, 'CourseName': res[1], 'CourseDesc': res[2]}
 
-      # Look up every id in the cards and append the overal rating to it
-      for id in cards:
-        mycursor.callproc("getOverallRating", args=(id, instructorid))
-        for result in mycursor.stored_results():
-          cards[id]['OverallRating'] = result.fetchall()[0][0]
-    
-    else:
-      
-      # note when user searches for a course name this the Course.idCourse will be the same for all the returned values
-      # this is how the sql is set up!
-      mycursor.callproc("returnAllClasses", args=(query,))
-      for result in mycursor.stored_results():
-        for i, res in enumerate(result):
-          name = " ".join(res[0:2]) # joins the first and last name
-          courseName = res[2]
-          overallrating = None # res[3] later be replaced by this 
-          cards[i] = {'ProfessorName': name, 'CourseName': courseName, 'OverallRating': overallrating}
+            # Look up every id in the cards and append the overal rating to it
+            for id in cards:
+                mycursor.callproc("getOverallRating", args=(id, instructorid))
+                for result in mycursor.stored_results():
+                    cards[id]['OverallRating'] = result.fetchall()[0][0]
 
-      print(cards)
-    
-    return render_template("search.html", cards=cards)
-  
-  return render_template("search.html")
+        else:
+
+            # note when user searches for a course name this the Course.idCourse will be the same for all the returned values
+            # this is how the sql is set up!
+            mycursor.callproc("returnAllClasses", args=(query,))
+            for result in mycursor.stored_results():
+                for i, res in enumerate(result):
+                    name = " ".join(res[0:2])  # joins the first and last name
+                    courseName = res[2]
+                    overallrating = None  # res[3] later be replaced by this
+                    cards[i] = {
+                        'ProfessorName': name, 'CourseName': courseName, 'OverallRating': overallrating}
+
+            print(cards)
+
+        return render_template("search.html", cards=cards)
+
+    return render_template("search.html")
 
 
 @app.route("/professor/<fname>-<lname>-<term>-<year>-<course>")
@@ -135,10 +140,15 @@ def submitted():
 # Professor card page routing
 @app.route("/professorCard")
 def card():
-  legend = 'Grade Disbursements'
-  labels = ['A', 'B', 'C', 'D', 'F']
-  values = [10, 18, 8, 5, 3]
-  return render_template('professorCard.html', values=values, labels=labels, legend=legend)
+    legend = 'Grade Disbursements'
+    labels = ['A', 'B', 'C', 'D', 'F']
+    values = [10, 18, 8, 5, 3]
+    return render_template('professorCard.html', values=values, labels=labels, legend=legend)
+
+
+@app.route("/aboutUs")
+def aboutus():
+    return render_template("aboutUs.html")
 
 
 # Run if main
