7ae3ee241cbb2a5137981b7b8fa9b66930149296
jessicamargala <56134693+jessicamargala@users.noreply.github.com>
2022-05-09 11:35:19 -0700

    adds thank you page

--- ratemybronco/__init__.py
+++ ratemybronco/__init__.py
@@ -134,7 +134,7 @@ def add_rating():
 # Thank you page or the submitted page
 @app.route("/submitted", methods=["GET"])
 def submitted():
-    return "Thank You"
+    return render_template("thankYou.html")
 
 
 # Professor card page routing
